428496c3da7de4c4ddf6ae91c74748c4628eab95
Egor1712 <58880149+Egor1712@users.noreply.github.com>
2021-03-16 16:59:58 +0500

    Add fadeOut

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
+const anim = animaster();
 addListeners();
 
 function addListeners() {
     document.getElementById('fadeInPlay')
         .addEventListener('click', function () {
             const block = document.getElementById('fadeInBlock');
-            fadeIn(block, 5000);
+            anim.fadeIn(block, 5000);
         });
 
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
-            move(block, 1000, {x: 100, y: 10});
+            anim.move(block, 1000, {x: 100, y: 10});
         });
 
     document.getElementById('scalePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('scaleBlock');
-            scale(block, 1000, 1.25);
+            anim.scale(block, 1000, 1.25);
         });
+    document
+    .querySelector("#fadeOutPlay")
+    .addEventListener("click", function (){
+        const block = document.querySelector("#fadeOutBlock");
+        anim.fadeOut(block, 5000);
+    })
 }
 
 /**
@@ -25,11 +32,7 @@ function addListeners() {
  * @param element — HTMLElement, который надо анимировать
  * @param duration — Продолжительность анимации в миллисекундах
  */
-function fadeIn(element, duration) {
-    element.style.transitionDuration =  `${duration}ms`;
-    element.classList.remove('hide');
-    element.classList.add('show');
-}
+
 
 /**
  * Функция, передвигающая элемент
@@ -37,10 +40,7 @@ function fadeIn(element, duration) {
  * @param duration — Продолжительность анимации в миллисекундах
  * @param translation — объект с полями x и y, обозначающими смещение блока
  */
-function move(element, duration, translation) {
-    element.style.transitionDuration = `${duration}ms`;
-    element.style.transform = getTransform(translation, null);
-}
+
 
 /**
  * Функция, увеличивающая/уменьшающая элемент
@@ -48,10 +48,7 @@ function move(element, duration, translation) {
  * @param duration — Продолжительность анимации в миллисекундах
  * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
  */
-function scale(element, duration, ratio) {
-    element.style.transitionDuration =  `${duration}ms`;
-    element.style.transform = getTransform(null, ratio);
-}
+
 
 function getTransform(translation, ratio) {
     const result = [];
@@ -62,4 +59,31 @@ function getTransform(translation, ratio) {
         result.push(`scale(${ratio})`);
     }
     return result.join(' ');
+}
+
+function animaster(){
+    const scale = function scale(element, duration, ratio) {
+        element.style.transitionDuration =  `${duration}ms`;
+        element.style.transform = getTransform(null, ratio);
+    };
+    const move = function move(element, duration, translation) {
+        element.style.transitionDuration = `${duration}ms`;
+        element.style.transform = getTransform(translation, null);
+    };
+    const fadeIn = function fadeIn(element, duration) {
+        element.style.transitionDuration =  `${duration}ms`;
+        element.classList.remove('hide');
+        element.classList.add('show');
+    };
+    const fadeOut = function fadeOut(element, duration) {
+        element.style.transitionDuration = `${duration}ms`;
+        element.classList.remove("show");
+        element.classList.add("hide");
+    }
+    return {
+        scale,
+        move,
+        fadeIn,
+        fadeOut
+    }
 }
