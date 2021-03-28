b48f502c1e81fd4af274130ed00af80b8c1331e6
Евгений Куликов <9126612794@mail.ru>
2021-03-28 15:09:55 +0500

    1-4 full + refactor + refactor

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 addListeners();
 
-function animaster() {
-	let animaster = {};
-
+let Animaster = new class {
 	/**
 	 * Блок плавно появляется из прозрачного.
 	 * @param element — HTMLElement, который надо анимировать
 	 * @param duration — Продолжительность анимации в миллисекундах
 	 */
-	animaster.fadeIn = function (element, duration) {
+	fadeIn(element, duration) {
 		element.style.transitionDuration = `${duration}ms`;
 		element.classList.remove('hide');
 		element.classList.add('show');
@@ -19,7 +17,7 @@ function animaster() {
 	 * @param element — HTMLElement, который надо анимировать
 	 * @param duration — Продолжительность анимации в миллисекундах
 	 */
-	animaster.fadeOut = function (element, duration) {
+	fadeOut(element, duration) {
 		element.style.transitionDuration = `${duration}ms`;
 		element.classList.add('hide');
 		element.classList.remove('show');
@@ -31,9 +29,9 @@ function animaster() {
 	 * @param duration — Продолжительность анимации в миллисекундах
 	 * @param translation — объект с полями x и y, обозначающими смещение блока
 	 */
-	animaster.move = function (element, duration, translation) {
+	move(element, duration, translation) {
 		element.style.transitionDuration = `${duration}ms`;
-		element.style.transform = getTransform(translation, null);
+		element.style.transform = this._getTransform(translation, null);
 	}
 
 	/**
@@ -42,30 +40,30 @@ function animaster() {
 	 * @param duration — Продолжительность анимации в миллисекундах
 	 * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
 	 */
-	animaster.scale = function (element, duration, ratio) {
+	scale(element, duration, ratio) {
 		element.style.transitionDuration = `${duration}ms`;
-		element.style.transform = getTransform(null, ratio);
+		element.style.transform = this._getTransform(null, ratio);
 	}
 
-	animaster.moveAndHide = function (element, duration, translation) {
-		animaster.move(element, duration * 2 / 5, translation);
-		setTimeout(() => animaster.fadeOut(element, duration * 3 / 5), duration * 2 / 5);
+	moveAndHide(element, duration, translation) {
+		this.move(element, duration * 2 / 5, translation);
+		setTimeout(() => this.fadeOut(element, duration * 3 / 5), duration * 2 / 5);
 	}
 
-	animaster.showAndHide = function (element, duration) {
-		animaster.fadeIn(element, duration / 3);
-		setTimeout(() => animaster.fadeOut(element, duration / 3), duration * 2 / 3);
+	showAndHide(element, duration) {
+		this.fadeIn(element, duration / 3);
+		setTimeout(() => this.fadeOut(element, duration / 3), duration * 2 / 3);
 	}
 
-	animaster.heartBeating = function (element, duration, ratio) {
+	heartBeating(element, duration, ratio) {
 		let loop = () => {
-			animaster.scale(element, duration / 2, ratio);
-			setTimeout(() => animaster.scale(element, duration / 2, 1), duration / 2);
+			this.scale(element, duration / 2, ratio);
+			setTimeout(() => this.scale(element, duration / 2, 1), duration / 2);
 		}
 		setInterval(loop, duration);
 	}
 
-	function getTransform(translation, ratio) {
+	_getTransform(translation, ratio) {
 		const result = [];
 		if (translation) {
 			result.push(`translate(${translation.x}px,${translation.y}px)`);
@@ -75,43 +73,21 @@ function animaster() {
 		}
 		return result.join(' ');
 	}
-
-	return animaster;
 }
 
 function addListeners() {
-	document.getElementById('fadeInPlay').addEventListener('click', function () {
-		const block = document.getElementById('fadeInBlock');
-		animaster().fadeIn(block, 5000);
-	});
+	addListener('fadeIn', 2000);
+	addListener('fadeOut', 2000);
+	addListener('move',1000, {x: 100, y: 10});
+	addListener('scale', 1000, 1.25);
+	addListener('moveAndHide',1000, {x: 100, y: 20});
+	addListener('showAndHide', 1000);
+	addListener('heartBeating', 1000, 1.4);
+}
 
-	document.getElementById('fadeOutPlay').addEventListener('click', function () {
-		const block = document.getElementById('fadeOutBlock');
-		animaster().fadeOut(block, 5000);
-	});
-
-	document.getElementById('movePlay').addEventListener('click', function () {
-		const block = document.getElementById('moveBlock');
-		animaster().move(block, 1000, {x: 100, y: 10});
-	});
-
-	document.getElementById('scalePlay').addEventListener('click', function () {
-		const block = document.getElementById('scaleBlock');
-		animaster().scale(block, 1000, 1.25);
-	});
-
-	document.getElementById('moveAndHidePlay').addEventListener('click', function () {
-		const block = document.getElementById('moveAndHideBlock');
-		animaster().moveAndHide(block, 1000, {x: 100, y: 20});
-	});
-
-	document.getElementById('showAndHidePlay').addEventListener('click', function () {
-		const block = document.getElementById('showAndHideBlock');
-		animaster().showAndHide(block, 1000);
-	});
-
-	document.getElementById('heartBeatingPlay').addEventListener('click', function () {
-		const block = document.getElementById('heartBeatingBlock');
-		animaster().heartBeating(block, 1000, 1.4);
+function addListener(methodName, ...methodArguments) {
+	document.getElementById(`${methodName}Play`).addEventListener('click', function () {
+		const block = document.getElementById(`${methodName}Block`);
+		Animaster[methodName](block, ...methodArguments);
 	});
 }
